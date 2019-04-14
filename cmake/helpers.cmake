c0fd087042e8991ef40c79c5d2a135b8e168cec3
milerius <roman.sztergbaum@epitech.eu>
2019-04-14 18:41:00 +0200

    remove intrusive command and put a clear message if conan need to be executed

--- cmake/helpers.cmake
+++ cmake/helpers.cmake
@@ -1,15 +1,3 @@
-macro(exec_conan)
-    if (NOT EXISTS conanbuildinfo.cmake)
-        message("Conan need to be executed")
-        execute_process(COMMAND conan config install ../.conan/
-                RESULT_VARIABLE CMD_ERROR
-                OUTPUT_FILE CMD_OUTPUT)
-        execute_process(COMMAND conan install ../
-                RESULT_VARIABLE CMD_ERROR
-                OUTPUT_FILE CMD_OUTPUT)
-        MESSAGE(STATUS "CMD_ERROR:" ${CMD_ERROR})
-        MESSAGE(STATUS "CMD_OUTPUT:" ${CMD_OUTPUT})
-    else ()
-        message("Conan doesn't need to be executed")
-    endif ()
-endmacro()
+ if (NOT EXISTS conanbuildinfo.cmake)
+     message(FATAL_ERROR "Conan need to be executed: conan install <path_to_your_conanfile.txt>")
+ endif ()
